e966ab9c1b9826b45ead87b59856a49e633202cb
Troy Gooden <tro12185@gmail.com>
2019-03-25 22:36:14 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -22,40 +22,40 @@ def reformat_languages(languages)
   
 end
 
-def languages 
+# def languages 
   
-  languages = {
-    :oo => {
-      :ruby => {
-        :type => "interpreted"
-      },
-      :javascript => {
-        :type => "interpreted"
-      },
-      :python => {
-        :type => "interpreted"
-      },
-      :java => {
-        :type => "compiled"
-      }
-    },
-    :functional => {
-      :clojure => {
-        :type => "compiled"
-      },
-      :erlang => {
-        :type => "compiled"
-      },
-      :scala => {
-        :type => "compiled"
-      },
-      :javascript => {
-        :type => "interpreted"
-      }
-    }
-  }
+#   languages = {
+#     :oo => {
+#       :ruby => {
+#         :type => "interpreted"
+#       },
+#       :javascript => {
+#         :type => "interpreted"
+#       },
+#       :python => {
+#         :type => "interpreted"
+#       },
+#       :java => {
+#         :type => "compiled"
+#       }
+#     },
+#     :functional => {
+#       :clojure => {
+#         :type => "compiled"
+#       },
+#       :erlang => {
+#         :type => "compiled"
+#       },
+#       :scala => {
+#         :type => "compiled"
+#       },
+#       :javascript => {
+#         :type => "interpreted"
+#       }
+#     }
+#   }
   
   
   
-end 
-reformat_languages(languages)
+# end 
+# reformat_languages(languages)
